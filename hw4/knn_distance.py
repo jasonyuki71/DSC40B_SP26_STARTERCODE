ebbce173307aeae8bc5f457531db26cda58b6bd2
Yuki <jasonyuki71@gmail.com>
2026-04-30 13:22:00 -0700

    fix error

--- hw4/knn_distance.py
+++ hw4/knn_distance.py
@@ -4,21 +4,33 @@ def knn_distance(arr, q, k):
     """Compute the kth nearest point and the distance to it."""
     pairs = [(abs(x - q), x) for x in arr]
 
-    def quickselect(lst, k):
-        if len(lst) == 1:
-            return lst[0]
+    def partition(left, right, pivot_idx):
+        pivot = pairs[pivot_idx][0]
+        pairs[pivot_idx], pairs[right] = pairs[right], pairs[pivot_idx]
+        store = left
+
+        for i in range(left, right):
+            if pairs[i][0] < pivot:
+                pairs[store], pairs[i] = pairs[i], pairs[store]
+                store += 1
+        pairs[right], pairs[store] = pairs[store], pairs[right]
+
+        return store
+
+    def quickselect(left, right, k_smallest):
+        if left == right:
+            return
+
+        pivot_idx = random.randint(left, right)
+        pivot_idx = partition(left, right, pivot_idx)
         
-        pivot = lst[0]
-        left = [x for x in lst if x[0] < pivot[0]]
-        mid = [x for x in lst if x[0] == pivot[0]]
-        right = [x for x in lst if x[0] > pivot[0]]
-        
-        if k <= len(left):
-            return quickselect(left, k)
-        elif k <= len(left) + len(mid):
-            return mid[0] 
+        if k_smallest == pivot_idx:
+            return            
+        elif k_smallest < pivot_idx:
+            quickselect(left, pivot_idx - 1, k_smallest)
         else:
-            return quickselect(right, k - len(left) - len(mid))
+            quickselect(pivot_idx + 1, right, k_smallest)
+
+    quickselect(0, len(pairs) - 1, k - 1)
     
-    distance, point = quickselect(pairs, k)
-    return (distance, point)
+    return pairs[k - 1]       
